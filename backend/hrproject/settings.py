73f2c5f4c6fc2f5fc76ae862ceba360dfb9b090f
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-12-16 17:33:35 +0300

    add cors

--- backend/hrproject/settings.py
+++ backend/hrproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'tinymce',
     'hrapp',
 
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'hrproject.urls'
