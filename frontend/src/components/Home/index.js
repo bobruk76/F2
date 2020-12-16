2a56221e4dcdb8d2ded6cf391239f41453b3a5fc
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-12-16 13:48:45 +0300

    change vue settings

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'http://localhost:8000/api';
+//const BASE_API_URL = 'http://localhost:8000/api';
+const BASE_API_URL = '/api';
 
 export default {
   data() {
