3e3df6d6cabbbde387bb0d9d0f8ff9beb19eadf1
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-12-17 20:20:46 +0300

    chang nginx settigs

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'http://localhost:8000/api';
-//const BASE_API_URL = '/api';
+//const BASE_API_URL = 'http://localhost:8000/api';
+const BASE_API_URL = '/api';
 
 export default {
   data() {
